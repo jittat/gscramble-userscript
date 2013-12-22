05cdcd45d13d894d1e2af1477e4fd86ab5f3c7dd
Jittat Fakcharoenphol <jittat@gmail.com>
2013-12-22 21:29:31 +0700

    scrambles search results

--- gscramble.user.js
+++ gscramble.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Google Scramble
-// @namespace    googleScramble
+// @namespace    https://github.com/jittat/gscramble-userscript
 // @include      https://www.google.com/search?*q=*
 // @author       Jittat Fakcharoenphol
 // @description  This user script scrambles google ranking.
@@ -21,9 +21,27 @@
   
   var main = function() {
     
+    var shuffle = function(array) {
+      // from: http://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array-in-javascript
+      var counter = array.length, temp, index;
+      while (counter > 0) {
+        index = Math.floor(Math.random() * counter);
+        counter--;
+        temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+      }
+      return array;
+    };
     
-    setTimeout(function() { alert(jQ("#ires li.g").length); },
-               2000);
+    var resultItems = jQ("#ires li.g");
+    var parent = resultItems.first().parent();
+    
+    resultItems.detach();
+    shuffle(resultItems);
+    jQ.each(resultItems, function(i,elt) {
+      parent.append(elt);
+    });
   };
   
   addJQuery(main);
